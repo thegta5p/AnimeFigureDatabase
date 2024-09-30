116364f1b15efc8e84d493a25be2c6c3338b27dd
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2024-09-29 18:40:19 -0700

    added in github repo button to footer

--- anime-figure-database/app/layout.tsx
+++ anime-figure-database/app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import {Footer} from "@/components/footer";
 
 export const metadata: Metadata = {
   title: {
@@ -48,15 +49,7 @@ export default function RootLayout({
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
+              <Footer/>
             </footer>
           </div>
         </Providers>
